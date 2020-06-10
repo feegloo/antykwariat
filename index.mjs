53d66cd58ceb197aac31b637d54cfc5ea5034c11
Aleksander Figiel <aleksander.figiel@rtbhouse.com>
2020-06-10 13:21:47 +0200

    push

--- index.mjs
+++ index.mjs
@@ -1,16 +1,44 @@
 import fetch from 'node-fetch'
+import koa from 'koa'
+import koaRouter from 'koa-router'
+import koaJson from 'koa-json'
 
-(async () => {
-    fetch('https://api2.isbndb.com/book/9788380080157', {
+// (async () => {
+//     fetch('https://api2.isbndb.com/book/9788380080157', {
+//         method: 'get',
+//         headers: {
+//             "Content-Type": 'application/json', 
+//             'Authorization': '44151_dbd862b620e23d6ab5c3971156ce35a9' 
+//         },
+//     })
+//     .then(res => res.json())
+//     .then(json => console.log(json));
+// })()
+
+// cd $HOME/.local/bin
+// ./isbn_meta 9788380080157
+
+const app = new koa()
+
+const router = new koaRouter()
+
+router.get('/:id', async (ctx) => {
+    const response = await fetch('https://api2.isbndb.com/book/' + ctx.params.id, {
         method: 'get',
         headers: {
             "Content-Type": 'application/json', 
             'Authorization': '44151_dbd862b620e23d6ab5c3971156ce35a9' 
         },
     })
-    .then(res => res.json())
-    .then(json => console.log(json));
-})()
 
-// cd $HOME/.local/bin
-// ./isbn_meta 9788380080157
+    const json = await response.json()
+
+    ctx.body = json
+})
+
+
+app.use(koaJson())
+app.use(router.routes())
+
+
+app.listen(8080, () => console.log('running on port 8080'))
